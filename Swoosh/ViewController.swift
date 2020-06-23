28f5c2805ee3fe7224e350a181e775e28d7f16eb
Roman <pondz.rf@mail.ru>
2020-06-23 17:46:35 +0300

    first commit

---
+++ Swoosh/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swoosh
+//
+//  Created by Roman Tuzhilkin on 6/23/20.
+//  Copyright © 2020 Roman Tuzhilkin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
